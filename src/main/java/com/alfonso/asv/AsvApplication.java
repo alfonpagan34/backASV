3b6d54827e1960d97125cb5b354f9304d4af9c68
Alfonso Pagán <compilalfon@gmail.com>
2023-09-25 17:27:02 +0200

    first commit

---
+++ src/main/java/com/alfonso/asv/AsvApplication.java
@@ -0,0 +1,13 @@
+package com.alfonso.asv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alfonso/asv/AsvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alfonso.asv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
